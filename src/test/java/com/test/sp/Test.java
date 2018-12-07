416023c40d8810f02185e1492cd33186c4364439
KOITT <KOITT@KOITT-PC>
2018-12-07 19:54:25 +0900

    asdf

--- src/test/java/com/test/sp/Test.java
+++ src/test/java/com/test/sp/Test.java
@@ -22,9 +22,8 @@ public class Test {
 		for(int i=0;i<str1.length;i++) {
 			for(int j=0;j<str2.length;j++) {
 				if(str1[i]==str2[j]) {
-					
+					str1[i].split(str2[j]);
 				}
-				str1[i].split(str2[j]);
 			}
 		}
 		return str1;
@@ -46,3 +45,24 @@ public class Test {
 		System.out.println(sli(str1,str2));
 	}
 }
+
+/*class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        int i=0;
+        int cnt = 0;
+        for(int j = 0;j<completion.length;j++){
+            if(participant[i]==completion[j]){
+                cnt += 1;   
+            }else{
+                cnt += 0;
+            }
+            if(cnt==0){
+                answer = participant[i];
+            }
+            cnt = 0;
+            i++;
+        }
+        return answer;
+    }
+}*/
